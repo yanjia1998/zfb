76967ee88bb9c44667837e28f56db7cfa3dff045
yanjia <475581796@qq.com>
2020-12-09 18:20:29 +0800

    zfb

--- src/main/java/com/qf/dao/GoodsRepository.java
+++ src/main/java/com/qf/dao/GoodsRepository.java
@@ -2,7 +2,6 @@ package com.qf.dao;
 
 import com.qf.pojo.Goods;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.utils.Date;
 
 public interface GoodsRepository  extends JpaRepository<Goods,Integer> {
 }
