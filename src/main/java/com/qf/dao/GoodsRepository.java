71c10eb7d92bd6d3150920d3e6e51bef453ee67c
11 <123456@qq.com>
2020-12-09 20:10:23 +0800

    xt提交测试1

--- src/main/java/com/qf/dao/GoodsRepository.java
+++ src/main/java/com/qf/dao/GoodsRepository.java
@@ -4,5 +4,5 @@ import com.qf.pojo.Goods;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GoodsRepository  extends JpaRepository<Goods,Integer> {
-    //提交
+    //提交test
 }
